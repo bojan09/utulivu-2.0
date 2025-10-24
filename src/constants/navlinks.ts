6dac3db12c7fc67cc72f9bb1dd584e5a5fa07693
Bojan Stanimirovski <bojan-stanimirovski@097@hotmail.com>
2025-10-24 16:48:41 +0200

    commit

--- src/constants/navlinks.ts
+++ src/constants/navlinks.ts
@@ -2,7 +2,7 @@ export const navLinks = [
   { href: "/", label: "Home" },
   { href: "/#destinations", label: "Destinations" },
   { href: "/#testimonials", label: "Testimonials" },
-  { href: "#faq", label: "Faq" },
-  { href: "#about", label: "About Us" },
-  { href: "#contact", label: "Contact Us" },
+  { href: "/#faq", label: "Faq" },
+  { href: "/#about", label: "About Us" },
+  { href: "/#contact", label: "Contact Us" },
 ];
