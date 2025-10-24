f49297780bd3ce82fe5075d71f2763ffeb0a70a7
Bojan Stanimirovski <bojan-stanimirovski@097@hotmail.com>
2025-10-24 16:48:25 +0200

    navbar - navlinks minor bug fix

--- src/constants/navlinks.ts
+++ src/constants/navlinks.ts
@@ -1,7 +1,7 @@
 export const navLinks = [
-  { href: "#home", label: "Home" },
-  { href: "#destinations", label: "Destinations" },
-  { href: "#testimonials", label: "Testimonials" },
+  { href: "/", label: "Home" },
+  { href: "/#destinations", label: "Destinations" },
+  { href: "/#testimonials", label: "Testimonials" },
   { href: "#faq", label: "Faq" },
   { href: "#about", label: "About Us" },
   { href: "#contact", label: "Contact Us" },
